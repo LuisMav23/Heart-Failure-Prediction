99e7c00363ccf5e4d49eb43b23da7ae6203736d0
Mav023 <gabrielluismaverick@gmail.com>
2023-07-20 01:00:08 +0800

    preapared kNN model

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\luis\Desktop\Programs\Data-Science-Projects\Heart_Failure_Prediction\venv
+set VIRTUAL_ENV=C:\Users\luis\Desktop\Programs\Data-Science-Projects\Heart-Failure-Prediction\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
